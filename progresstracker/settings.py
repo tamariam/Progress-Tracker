b487a55b90041d25c99cb0dad23d1c980735dbff
Sopiko Abulidze <sabulidze@meathcoco.ie>
2025-10-30 14:20:47 +0000

    configure urls, create basic view, and template for testing purposes

--- progresstracker/settings.py
+++ progresstracker/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker_app.apps.TrackerAppConfig',
+    
 ]
 
 MIDDLEWARE = [
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from.models import Theme, Action    
+
+
 
 # Create your views here.
+def home(request):
+    print("--- The 'home' view is being executed. ---")
+    themes = Theme.objects.all().prefetch_related('actions')
+    return render(request, 'tracker_app/home.html', {'themes': themes})
