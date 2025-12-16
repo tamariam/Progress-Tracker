0478bbbffa2d87024da8ae599516be65f6071e6c
Sopiko Abulidze <sabulidze@meathcoco.ie>
2025-12-16 16:22:32 +0000

    add scroll-up functionality

--- tracker_app/static/js/modal.js
+++ tracker_app/static/js/modal.js
@@ -248,3 +248,24 @@ function closeMCCModal() {
     const modal = bootstrap.Modal.getOrCreateInstance(modalElement);
     modal.hide();
 }
+
+// Scroll to Top Button Functionality
+// 1. Get the button element
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// 2. Handle visibility on scroll
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+// 3. Smooth scroll logic
+function scrollToTopFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
