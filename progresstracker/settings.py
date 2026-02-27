ab144185ecbbb8b4b0e8ebe9109b0b6df3cffa0e
Sopiko Abulidze <sabulidze@meathcoco.ie>
2026-02-27 11:10:34 +0000

    dsjhdaskjhd

--- progresstracker/settings.py
+++ progresstracker/settings.py
@@ -41,7 +41,7 @@ DEBUG = True
 # settings.py
 ALLOWED_HOSTS = os.getenv(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost,progresstracker"
+    "127.0.0.1,localhost,10.55.214.65,bh-appsvrtest"
 ).split(",")
 
 
