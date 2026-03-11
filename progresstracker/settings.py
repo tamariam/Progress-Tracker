87ddef44a67c268dd4a7953fcbf6a7913c7f8551
Sopiko Abulidze <sabulidze@meathcoco.ie>
2026-03-11 12:02:08 +0000

    update email settings

--- progresstracker/settings.py
+++ progresstracker/settings.py
@@ -172,16 +172,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = os.environ.get('SMTP_HOST', 'smtp-mail.outlook.com')
-EMAIL_PORT = int(os.environ.get('SMTP_PORT', 587))
-
+EMAIL_HOST = "smtp.office365.com"
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 
-EMAIL_HOST_USER = os.environ.get('SMTP_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 
 
